c8b78d0be281dda8c202302f3f722896dd53cbcc
Andhika Fadhlan Wijanarko <andhikafadhlanw@gmail.com>
2024-02-10 01:04:54 +0800

    update main.py

--- main.py
+++ main.py
@@ -43,14 +43,6 @@ async def create_upload_files(question: str = Form(...),files: list[UploadFile] 
         service_context = ServiceContext.from_defaults(llm=llm, chunk_size=1000, chunk_overlap=20)
         #set_global_service_context(service_context)                                              
         index = VectorStoreIndex.from_documents(documents, service_context=service_context)       
- 
-        #Storing and Loading the Index                                         
-        index.storage_context.persist()                                        
-                                                                       
-        from llama_index import StorageContext, load_index_from_storage        
-                                                                       
-        storage_context = StorageContext.from_defaults(persist_dir="./storage")
-        index = load_index_from_storage(storage_context=storage_context)                                         
         query_engine = index.as_query_engine()                                                    
                                                                                           
         response = query_engine.query(question)                                                   
